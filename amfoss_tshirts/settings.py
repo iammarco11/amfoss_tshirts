d537eb1f4845085a0066ebbfbc464b6caefab32d
akshay <akshaypraveennair11@gmail.com>
2019-11-13 00:16:04 +0530

    make some changes

--- amfoss_tshirts/settings.py
+++ amfoss_tshirts/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -132,10 +132,8 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = (
     os.path.join(BASE_DIR, 'media')
-import dj-database-url 
-prod_db  =  dj-database-url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
 
 )
 LOGIN_URL = '/tshirt/user_login/'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+django_heroku.settings(locals())
