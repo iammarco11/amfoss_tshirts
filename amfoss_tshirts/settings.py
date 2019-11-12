87184968f76fb568ff772926ff1662c6a4d7eef7
akshay <akshaypraveennair11@gmail.com>
2019-11-12 23:28:32 +0530

    update req.txt

--- amfoss_tshirts/settings.py
+++ amfoss_tshirts/settings.py
@@ -132,8 +132,8 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = (
     os.path.join(BASE_DIR, 'media')
-import dj_database_url 
-prod_db  =  dj_database_url.config(conn_max_age=500)
+import dj-database-url 
+prod_db  =  dj-database-url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
 )
