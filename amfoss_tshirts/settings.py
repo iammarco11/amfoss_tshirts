f50171b48c1c8057898aff49499086beeb580c1b
akshay <akshaypraveennair11@gmail.com>
2019-11-13 00:19:52 +0530

    update settings.py

--- amfoss_tshirts/settings.py
+++ amfoss_tshirts/settings.py
@@ -124,8 +124,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATICFILES_DIRS = [
 	os.path.join(PROJECT_ROOT, 'static'),
 ]
